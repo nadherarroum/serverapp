dcf4f31aeec3f97c8e143d3157e1c6ceea002ee1
Nadher Arroum <nadher.arroum@gmail.com>
2022-06-06 17:58:33 +0100

    implement models(interfaces) & appstate

---
+++ src/app/interface/server.ts
@@ -0,0 +1,10 @@
+import { Status } from '../enum/status.enum';
+
+export interface Server {
+  id: number;
+  ipAddress: string;
+  name: string;
+  memory: string;
+  type: string;
+  status: Status;
+}
