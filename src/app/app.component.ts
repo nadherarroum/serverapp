4d138783fa1cbefb6b3a347ef8eae22fb51e40dc
Nadher Arroum <nadher.arroum@gmail.com>
2022-06-06 19:27:51 +0100

    Subscribe to the service from the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { DataState } from './enum/data-state.enum';
+import { CustomResponse } from './interface/custom-response';
+import { AppState } from './interface/app-state';
+import { Observable, map, startWith, catchError, of } from 'rxjs';
+import { ServerService } from './service/server.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'serverapp';
+export class AppComponent implements OnInit {
+  appState$: Observable<AppState<CustomResponse>>;
+
+  constructor(private serverService: ServerService) {}
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$.pipe(
+      map((response) => {
+        return { dataState: DataState.LOADED_STATE, appData: response };
+      }),
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error: error });
+      })
+    );
+  }
 }
